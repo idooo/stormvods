eccc0adaeb75c147bbbba2bcd806445efe221760
Alex Shteinikov <aleksandr.shteinikov@autogeneral.com.au>
2015-11-12 15:55:21 +1000

    minor things

--- server/routes/test.js
+++ server/routes/test.js
@@ -5,20 +5,18 @@ var Route = require('./abstract.route');
 class TestRoute extends Route {
 
 	configure() {
-		var self = this;
-
-		self.server.get('/api/data', function (req, res, next) {
-
-			self.model.Datasource.getDatasources()
-				.then(function(datasources) {
-					Route.success(res, datasources);
-					return next();
-				})
-				.catch(function(err) {
-					Route.fail(res, err);
-					return next();
-				});
-		})
+		this.server.get('/api/data', this.routeData.bind(this))
+	}
+	routeData(req, res, next) {
+		this.model.Datasource.getDatasources()
+			.then(function(datasources) {
+				Route.success(res, datasources);
+				return next();
+			})
+			.catch(function(err) {
+				Route.fail(res, err);
+				return next();
+			});
 	}
 }
 
