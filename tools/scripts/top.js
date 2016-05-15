4df121ad3a3e8cbe3e03ebf139f40eecc90df05d
Alex Shteinikov <alex.shteinikov@gmail.com>
2016-05-15 15:45:34 +1000

    top script improvements

--- tools/scripts/top.js
+++ tools/scripts/top.js
@@ -2,26 +2,24 @@
 
 /**
  * Generate lists of top videos
- * 
- * -c (path to config) 
- * 
+ *
+ * -c (path to config)
+ *
  * ./role.js -c config/default.json
- * 
+ *
  */
 
 var path = require('path'),
 	moment = require('moment'),
 	argv = require('yargs').argv,
 	Database = require('../../server/core/database');
-	
+
 var tops = {
 	DAY1: moment().subtract(1, 'days').toDate(),
 	DAY7: moment().subtract(7, 'days').toDate(),
 	DAY30: moment().subtract(30, 'days').toDate(),
 	ALLTIME: 0
-}
-
-const LIMIT = 20;
+};
 
 if (!argv.c) {
 	return console.error('-c argument must be specified');
@@ -30,44 +28,57 @@ if (!argv.c) {
 var configPath = path.join(process.cwd(), argv.c);
 
 try {
-	this.config = require(configPath)
+	this.config = require(configPath);
 }
 catch (e) {
-	return console.error(`Incorrect config path "${configPath}"`)
+	return console.error(`Incorrect config path "${configPath}"`);
 }
 
 var db = new Database(this.config.database),
 	models = db.loadModel(),
 	promises = [];
-	
+
 console.log('Timeframes:');
 console.log(tops);
 
 Object.keys(tops).forEach(function (key) {
 	var query = {isRemoved: false};
-	if (tops[key] !== 0) query.creationDate = {'$gt': tops[key]}
-	promises.push(models.Video.getList(query, '_id', {'rating': -1}, 20))
-})
+	if (tops[key] !== 0) query.creationDate = {'$gt': tops[key]};
+	promises.push(models.Video
+		.find(query, '_id')
+		.limit(20)
+		.sort({rating: -1}));
+});
 
-Promise.all(promises)
-	.then(function (response) {
+var oldTops;
+
+models.Tops.findOne({})
+	.then(_oldTops => {
+		oldTops = _oldTops;
+		return Promise.all(promises);
+	})
+	.then(response => {
 		var data = {
 			creationDate: new Date()
 		};
-		
-		for (var i = 0; i < response.length; i++) { 
-			data[Object.keys(tops)[i]] = response[i];
+
+		for (var i = 0; i < response.length; i++) {
+			if (response[i] && response[i].length > 0) {
+				data[Object.keys(tops)[i]] = response[i];
+			}
+			else {
+				console.log(`No new data for ${Object.keys(tops)[i]}`);
+				data[Object.keys(tops)[i]] = oldTops[Object.keys(tops)[i]];
+			}
 		}
-		
+
 		console.log('Updated tops will be written to DB:');
 		console.log(data);
-		
-		return models.Tops.updateOne({}, data, {upsert: true});
+
+		return models.Tops.update({}, data, {upsert: true});
 	})
-	.then(function (response) {
-		db.disconnect();
-	})
-	.catch(function (err) {
+	.then(db.disconnect)
+	.catch(err => {
 		console.error('Database error', err);
 		db.disconnect();
 	});
