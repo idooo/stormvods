def87e6e1833144e9e01a86d71e440f46ec98018
Alex Shteinikov <alex.shteinikov@gmail.com>
2016-06-05 17:54:38 +1000

    added twitter/youtube channel support for casters to the model

--- server/models/caster.model.js
+++ server/models/caster.model.js
@@ -1,9 +1,50 @@
 'use strict';
 
-var BasicModel = require('./basic.model');
+var mongoose = require('mongoose'),
+	uniqueValidator = require('mongoose-unique-validator'),
+	mongoosePaginate = require('mongoose-paginate'),
+	BasicModel = require('./basic.model'),
+	Constants = require('../constants');
 
 class Caster extends BasicModel {
 
+	constructor () {
+		super();
+		this.schema = new mongoose.Schema({
+			name: {
+				type: String,
+				trim: true,
+				unique: true,
+				index: true,
+				required: true,
+				minlength: BasicModel.constants().MIN_LENGTH
+			},
+			creationDate: {
+				type: Date,
+				default: Date.now
+			},
+			isRemoved: {
+				type: Boolean,
+				default: false
+			},
+			twitter: {
+				type: String,
+				trim: true
+			},
+			youtube: {
+				type: String,
+				trim: true
+			},
+			author: {
+				type: mongoose.Schema.Types.ObjectId,
+				required: true
+			}
+		});
+
+		this.schema.plugin(uniqueValidator, {message: Constants.ERROR_UNIQUE});
+		this.schema.plugin(mongoosePaginate);
+		this.schema.statics.getOrCreate = this.getOrCreate;
+	}
 }
 
 module.exports = Caster;
