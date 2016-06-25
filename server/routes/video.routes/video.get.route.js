8c703fedf5d94f399bdfd55a9c5a85b143c46159
Alex Shteinikov <alex.shteinikov@gmail.com>
2016-06-25 22:06:50 +1000

    minor bugfixes for statistics sidebar

--- server/routes/video.routes/video.get.route.js
+++ server/routes/video.routes/video.get.route.js
@@ -148,7 +148,7 @@ class VideoGetRoute {
 								rating: video.tournament[0] ? video.tournament[0].rating : null,
 								masterleagueId: data[i].masterleagueId,
 								hotslogsId: data[i].hotslogsId,
-								teamliquidId: data[i].teamliquidId
+								teamLiquidWikiUrl: data[i].teamLiquidWikiUrl
 							};
 							break;
 
